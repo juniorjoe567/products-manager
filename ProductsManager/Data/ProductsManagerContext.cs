b1c268eb33b35931623cdd6c1d16173aa14950e5
rukeijakare <juniorjoe567@gmail.com>
2024-09-11 09:51:36 +0300

    Add project files.

---
+++ ProductsManager/Data/ProductsManagerContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsManager.Models;
+
+namespace ProductsManager.Data
+{
+    public class ProductsManagerContext : DbContext
+    {
+        public ProductsManagerContext(DbContextOptions<ProductsManagerContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
